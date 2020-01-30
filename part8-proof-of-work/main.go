a605ec052c408a1580b8d7e1b11ebca81305446f
罗东云 <1052430943@qq.com>
2020-01-30 11:01:59 +0800

    判断区块Hash的有效性

---
+++ part8-proof-of-work/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"go-publicChain-study/part8-proof-of-work/BLC"
+)
+
+func main() {
+	////创世区块
+	//blockchain := BLC.CreateBlockchainWithGenesisBlock()
+	////新区块
+	//blockchain.AddBlockToBlockchain("Send 100RMB To zhangqiang", blockchain.Blocks[len(blockchain.Blocks)-1].Height+1, blockchain.Blocks[len(blockchain.Blocks)-1].Hash)
+	//
+	//blockchain.AddBlockToBlockchain("Send 300RMB To zhangqiang", blockchain.Blocks[len(blockchain.Blocks)-1].Height+1, blockchain.Blocks[len(blockchain.Blocks)-1].Hash)
+	//
+	//blockchain.AddBlockToBlockchain("Send 200RMB To zhangqiang", blockchain.Blocks[len(blockchain.Blocks)-1].Height+1, blockchain.Blocks[len(blockchain.Blocks)-1].Hash)
+	//
+	//blockchain.AddBlockToBlockchain("Send 300RMB To zhangqiang", blockchain.Blocks[len(blockchain.Blocks)-1].Height+1, blockchain.Blocks[len(blockchain.Blocks)-1].Hash)
+	//
+	//
+	//fmt.Println(blockchain.Blocks)
+
+	block := BLC.NewBlock("Test",1,[]byte{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0})
+
+	fmt.Printf("%d\n",block.Nonce)
+	fmt.Printf("%x\n",block.Hash)
+
+	proofOfWork :=  BLC.NewProofOfWork(block)
+	fmt.Printf("%v",proofOfWork.IsValid())
+
+
+
+
+
+}
