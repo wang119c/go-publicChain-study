7f45ac369c2b83ee64b60b60c1e590d872ae3d76
罗东云 <1052430943@qq.com>
2020-01-30 18:50:39 +0800

    通过迭代器遍历区块链

--- part19-persistence-iterator/BLC/Blockchain.go
+++ part19-persistence-iterator/BLC/Blockchain.go
@@ -18,69 +18,11 @@ type Blockchain struct {
 	DB  *bolt.DB
 }
 
-//迭代器
-func (blockchain *Blockchain) Iterator() *BlockchainIterator {
-	return &BlockchainIterator{blockchain.Tip, blockchain.DB}
-}
-
-type BlockchainIterator struct {
-	CurrentHash []byte
-	DB          *bolt.DB
-}
-
-func (blockchainIterator *BlockchainIterator) Next() *Block {
-	var block *Block
-	err := blockchainIterator.DB.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(blockTableName))
-		if b != nil {
-			currentBlockBytes := b.Get(blockchainIterator.CurrentHash)
-			//获取到当前currentHash对应的区块
-			block = DeserializeBlock(currentBlockBytes)
-
-			//更新迭代器里面currentHash
-			blockchainIterator.CurrentHash = block.PrevBlockHash
-
-		}
-		return nil
-	})
-
-	if err != nil {
-		log.Panic(err)
-	}
-	return block
-}
-
-//遍历输出所有区块信息
-func (blc *Blockchain) Printchain() {
-	blockchainInterator := blc.Iterator()
-	for {
-		block := blockchainInterator.Next()
-
-		fmt.Printf("Height:%d\n", block.Height)
-		fmt.Printf("PrevBlockHash:%x\n", block.PrevBlockHash)
-		fmt.Printf("Data:%s\n", block.Data)
-		fmt.Printf("Timestamp:%s\n", time.Unix(block.Timestamp, 0).Format("2006-01-02 03:04:05 PM"))
-		fmt.Printf("Hash:%x\n", block.Hash)
-		fmt.Printf("Nonce:%d\n", block.Nonce)
-
-		fmt.Println()
-
-
-		var hashInt big.Int
-		hashInt.SetBytes(block.PrevBlockHash)
-
-		if big.NewInt(0).Cmp(&hashInt) == 0 {
-			break
-		}
 
 
 
 
-
-	}
-}
-
-func (blc *Blockchain) PrintChain1() {
+func (blc *Blockchain) PrintChain() {
 	var block *Block
 	var currentHash []byte = blc.Tip
 	for {
@@ -94,7 +36,7 @@ func (blc *Blockchain) PrintChain1() {
 				fmt.Printf("Height:%d\n", block.Height)
 				fmt.Printf("PrevBlockHash:%x\n", block.PrevBlockHash)
 				fmt.Printf("Data:%s\n", block.Data)
-				fmt.Printf("Timestamp:%s\n", time.Unix(block.Timestamp, 0).Format("2006-01-02 03:04:05 PM"))
+				fmt.Printf("Timestamp:%s\n",  time.Unix(block.Timestamp , 0 ).Format("2006-01-02 03:04:05 PM")   )
 				fmt.Printf("Hash:%x\n", block.Hash)
 				fmt.Printf("Nonce:%d\n", block.Nonce)
 
---
+++ part19-persistence-iterator/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"go-publicChain-study/part18-persistence-iterator-time-format/BLC"
+)
+
+func main() {
+	//创世区块
+	blockchain := BLC.CreateBlockchainWithGenesisBlock()
+	defer blockchain.DB.Close()
+	//新区块
+	blockchain.AddBlockToBlockchain("Send 100RMB To zhangqiang")
+	blockchain.AddBlockToBlockchain("Send 300RMB To zhangqiang")
+	blockchain.AddBlockToBlockchain("Send 200RMB To zhangqiang")
+	blockchain.AddBlockToBlockchain("Send 300RMB To zhangqiang")
+
+
+	blockchain.Printchain()
+
+}
