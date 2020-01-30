bbcc2ec574c1778769b145ec8ba10f0d75317e16
罗东云 <1052430943@qq.com>
2020-01-30 19:50:13 +0800

    flag 及 os  使用

---
+++ part21-cli-flag-os/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	//flagString := flag.String("printChain", "", "输出所有的区块")
+	//flagInt := flag.Int("number", 6, "输出一个整数...")
+	//flagBool := flag.Bool("open",false,"判断真假...")
+	//flag.Parse()
+	//fmt.Printf("%s\n", *flagString)
+	//fmt.Printf("%d\n", *flagInt)
+	//fmt.Printf("%v\n", *flagBool)
+
+
+	args := os.Args
+	fmt.Printf("%v\n",args)
+	fmt.Printf("%v\n",args[1])
+
+}
