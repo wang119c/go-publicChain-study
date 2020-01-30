54df3aef2200cf841ceb9c33bc423e43a7af8131
罗东云 <1052430943@qq.com>
2020-01-30 12:44:36 +0800

    区块的序列化和反序列化

---
+++ part9-Serialize-DeserializeBlock/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"go-publicChain-study/part9-Serialize-DeserializeBlock/BLC"
+)
+
+func main() {
+	////创世区块
+	//blockchain := BLC.CreateBlockchainWithGenesisBlock()
+	////新区块
+	//blockchain.AddBlockToBlockchain("Send 100RMB To zhangqiang", blockchain.Blocks[len(blockchain.Blocks)-1].Height+1, blockchain.Blocks[len(blockchain.Blocks)-1].Hash)
+	//
+	//blockchain.AddBlockToBlockchain("Send 300RMB To zhangqiang", blockchain.Blocks[len(blockchain.Blocks)-1].Height+1, blockchain.Blocks[len(blockchain.Blocks)-1].Hash)
+	//
+	//blockchain.AddBlockToBlockchain("Send 200RMB To zhangqiang", blockchain.Blocks[len(blockchain.Blocks)-1].Height+1, blockchain.Blocks[len(blockchain.Blocks)-1].Hash)
+	//
+	//blockchain.AddBlockToBlockchain("Send 300RMB To zhangqiang", blockchain.Blocks[len(blockchain.Blocks)-1].Height+1, blockchain.Blocks[len(blockchain.Blocks)-1].Hash)
+	//
+	//
+	//fmt.Println(blockchain.Blocks)
+
+	block := BLC.NewBlock("Test",1,[]byte{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0})
+
+	fmt.Printf("%d\n",block.Nonce)
+	fmt.Printf("%x\n",block.Hash)
+
+	//proofOfWork :=  BLC.NewProofOfWork(block)
+	//fmt.Printf("%v",proofOfWork.IsValid())
+
+	bytes :=   block.Serialize()
+	fmt.Println(bytes)
+
+	block =  BLC.DeserializeBlock(bytes)
+
+	fmt.Printf("%d\n",block.Nonce)
+	fmt.Printf("%x\n",block.Hash)
+
+
+
+
+
+}
