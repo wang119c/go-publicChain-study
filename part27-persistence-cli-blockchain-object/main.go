ba486903918f45e6caf3b00b906dfc64efbb85e5
罗东云 <1052430943@qq.com>
2020-01-31 19:58:49 +0800

    代码完善处理

---
+++ part27-persistence-cli-blockchain-object/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"go-publicChain-study/part27-persistence-cli-blockchain-object/BLC"
+)
+
+func main() {
+
+	cli := BLC.CLI{}
+	cli.Run()
+
+	////新区块
+	//blockchain.AddBlockToBlockchain("Send 100RMB To zhangqiang")
+	//blockchain.AddBlockToBlockchain("Send 300RMB To zhangqiang")
+	//blockchain.AddBlockToBlockchain("Send 200RMB To zhangqiang")
+	//blockchain.AddBlockToBlockchain("Send 300RMB To zhangqiang")
+	//
+	//blockchain.Printchain()
+
+}
