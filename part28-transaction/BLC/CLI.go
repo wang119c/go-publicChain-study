4a33521f16d237f84b5cbac33a75a2553a5ee3c7
罗东云 <1052430943@qq.com>
2020-02-01 12:04:17 +0800

    Block里面设置Transaction数组

---
+++ part28-transaction/BLC/CLI.go
@@ -0,0 +1,118 @@
+package BLC
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+type CLI struct {
+}
+
+func printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("\tcreateblockchain -data DATA -- 创建创世区块")
+	fmt.Println("\taddblock -data DATA -- 交易数据")
+	fmt.Println("\tprintchain -- 输出区块信息")
+}
+
+func isValidArgs() {
+	if len(os.Args) < 2 {
+		printUsage()
+		os.Exit(1)
+	}
+}
+
+func (cli *CLI) addBlock(txs []*Transaction) {
+	if dbExists() == false {
+		fmt.Println("数据库不存在...")
+		os.Exit(1)
+	}
+
+	blockchain := BlockchainObject()
+	blockchain.AddBlockToBlockchain(txs)
+
+	blockchain.DB.Close()
+}
+
+func (cli *CLI) printchain() {
+
+	if dbExists() == false {
+		fmt.Println("数据库不存在...")
+		os.Exit(1)
+	}
+
+	blockchain := BlockchainObject()
+	blockchain.Printchain()
+
+	blockchain.DB.Close()
+}
+
+func (cli *CLI) createGenesisBlockchain(txs []*Transaction) {
+	CreateBlockchainWithGenesisBlock(txs)
+}
+
+func (cli *CLI) Run() {
+	isValidArgs()
+
+	addBlockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+	createBlockchainCmd := flag.NewFlagSet("createblockchain", flag.ExitOnError)
+
+	flagAddBlockData := addBlockCmd.String("data", "http://www.heilluo.com", "交易数据")
+	flagCreateBlockchainWithData := createBlockchainCmd.String("data", "Genesis Data...", "创建创世区块交易")
+
+	switch os.Args[1] {
+	case "addblock":
+		err := addBlockCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+		break
+	case "printchain":
+		err := printChainCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+		break
+	case "createblockchain":
+		err := createBlockchainCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+		break
+	default:
+		printUsage()
+		os.Exit(1)
+		break
+	}
+
+	if addBlockCmd.Parsed() {
+		if *flagAddBlockData == "" {
+			printUsage()
+			os.Exit(1)
+		}
+		fmt.Println(*flagAddBlockData)
+		cli.addBlock([]*Transaction{})
+	}
+
+	if printChainCmd.Parsed() {
+		//fmt.Println("输出区块所有数据")
+		cli.printchain()
+	}
+
+	if createBlockchainCmd.Parsed() {
+		if *flagCreateBlockchainWithData == "" {
+			fmt.Println("交易数据不能为空")
+			printUsage()
+			os.Exit(1)
+		}
+		cli.createGenesisBlockchain([]*Transaction{})
+
+		////创世区块
+		//blockchain := BLC.CreateBlockchainWithGenesisBlock()
+		//defer blockchain.DB.Close()
+	}
+
+}
---
+++ part28-transaction/BLC/Transaction.go
@@ -0,0 +1,8 @@
+package BLC
+
+//交易数据 UTXO
+type Transaction struct {
+	TxHash []byte //交易hash
+	//输入
+	//输出
+}
